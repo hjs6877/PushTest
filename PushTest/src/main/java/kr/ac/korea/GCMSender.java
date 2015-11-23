346ef51eec7a27ce4e70ae20a20065c82433e1a5
hjs6877 <hjs6877@gmail.com>
2015-11-23 22:30:00 +0900

    푸시 전송 연동

--- PushTest/src/main/java/kr/ac/korea/GCMSender.java
+++ PushTest/src/main/java/kr/ac/korea/GCMSender.java
@@ -1,39 +1,77 @@
 package kr.ac.korea;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by kjs on 2015-11-23.
  */
 public class GCMSender {
-    public static void main(String[] args){
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://storm-korea.cloudsc.kr");
+    public static final String API_KEY = "AIzaSyDV3uCP_9cw05IreXiBe7ur0Q-0oUMSnuI";
+
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 2 || args[0] == null) {
+            System.err.println("usage: ./gradlew run -Pmsg=\"MESSAGE\" [-Pto=\"DEVICE_TOKEN\"]");
+            System.err.println("");
+            System.err.println("Specify a test message to broadcast via GCM. If a device's GCM registration token is\n" +
+                    "specified, the message will only be sent to that device. Otherwise, the message \n" +
+                    "will be sent to all devices subscribed to the \"global\" topic.");
+            System.err.println("");
+            System.err.println("Example (Broadcast):\n" +
+                    "On Windows:   .\\gradlew.bat run -Pmsg=\"<Your_Message>\"\n" +
+                    "On Linux/Mac: ./gradlew run -Pmsg=\"<Your_Message>\"");
+            System.err.println("");
+            System.err.println("Example (Unicast):\n" +
+                    "On Windows:   .\\gradlew.bat run -Pmsg=\"<Your_Message>\" -Pto=\"<Your_Token>\"\n" +
+                    "On Linux/Mac: ./gradlew run -Pmsg=\"<Your_Message>\" -Pto=\"<Your_Token>\"");
+            System.exit(1);
+        }
         try {
-            CloseableHttpResponse response = httpClient.execute(httpPost);
-            HttpEntity entity = response.getEntity();
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-
-            System.out.println("status code: " + statusCode);
-            InputStream inputStream = entity.getContent();
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line = "";
-            while((line = br.readLine()) != null){
-                System.out.println(line);
+            // Prepare JSON containing the GCM message content. What to send and where to send.
+            JSONObject jGcmData = new JSONObject();
+            JSONObject jData = new JSONObject();
+            jData.put("message", args[0].trim());
+            // Where to send GCM message.
+            if (args.length > 1 && args[1] != null) {
+                jGcmData.put("to", args[1].trim());
+            } else {
+                jGcmData.put("to", "/topics/global");
             }
+            // What to send in GCM message.
+            jGcmData.put("data", jData);
+
+            // Create connection to send GCM Message request.
+            URL url = new URL("https://android.googleapis.com/gcm/send");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Authorization", "key=" + API_KEY);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            // Send GCM message content.
+            OutputStream outputStream = conn.getOutputStream();
+            outputStream.write(jGcmData.toString().getBytes());
+
+            // Read GCM response.
+            InputStream inputStream = conn.getInputStream();
+            String resp = IOUtils.toString(inputStream);
+            System.out.println(resp);
+            System.out.println("Check your device/emulator for notification or logcat for " +
+                    "confirmation of the receipt of the GCM message.");
         } catch (IOException e) {
+            System.out.println("Unable to send GCM message.");
+            System.out.println("Please ensure that API_KEY has been replaced by the server " +
+                    "API key, and that the device's registration token is correct (if specified).");
             e.printStackTrace();
         }
     }
